7435fd8e4f32eed47342fb45f5013b5c0d4d5e2e
rkasumi <r.kasumi@gmail.com>
2024-01-15 15:01:37 +0900

    fix

--- utils/merge_link.py
+++ utils/merge_link.py
@@ -1,13 +1,24 @@
 import json
+import plyvel
 import re
 
-outpath = "./output.json"
+legacyName = "equipment"
+compendiumName = "pf2e.equipment-srd.json"
+
+#pathLegacy = "/home/yaginumad/userdata_pf/Data/modules/pf2e-legacy-content/packs/"
+pathLegacy = "/home/yaginumad/userdata_pf/Data/systems/pf2e/packs/"
+pathCompendium = "../compendium/"
+
+json_l = {}
+ldb = plyvel.DB(pathLegacy+legacyName, create_if_missing=False)
+for key, line in ldb:
+  j = json.loads(line)
+  json_l[j['name']] = j
+ldb.close()
+
 json_c = None
-json_l = None
-with open("../compendium/pf2e.feats-srd.json", "r") as fh:
+with open(pathCompendium+compendiumName, "r") as fh:
   json_c = json.load(fh)
-with open("./feats-legacy.json", "r") as fh:
-  json_l = json.load(fh)
 
 for key in json_c['entries'].keys():
     if key in json_l:
@@ -23,10 +34,14 @@ for key in json_c['entries'].keys():
             uniq[m.groups()[0]] = 1
         for m in re.finditer(r'(\[\[/.*?\])[\s\.]', desc, re.MULTILINE):
             uniq[m.groups()[0]] = 1
+        for m in re.finditer(r'(\[\[/.*?\]\]\])', desc, re.MULTILINE):
+            uniq[m.groups()[0]] = 1
         
         for u in uniq.keys():
+            if 'description' not in json_c['entries'][key]:
+                continue
             if u not in json_c['entries'][key]['description']:
                 json_c['entries'][key]['description'] += '<p>'+u+'</p>'
 
-with open(outpath, 'w', encoding='utf-8') as fh:
+with open("./out-"+compendiumName, 'w', encoding='utf-8') as fh:
     json.dump(json_c, fh, indent=2, ensure_ascii=False)
